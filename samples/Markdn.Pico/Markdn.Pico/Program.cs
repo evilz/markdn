0fa01e3680218401f51d2a89215f8b7736b7dac0
Vincent B. <evilznet@gmail.com>
2025-11-14 09:19:14 +0100

    missing files

---
+++ samples/Markdn.Pico/Markdn.Pico/Program.cs
@@ -0,0 +1,39 @@
+using Markdn.Pico.Client.Pages;
+using Markdn.Pico.Components;
+using Markdn.Pico.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddScoped<IPostsService, PostsService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Markdn.Pico.Client._Imports).Assembly);
+
+app.Run();
